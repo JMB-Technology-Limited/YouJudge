8259fd52a4a90f261e3d84888199f02f93b2e749
James <james@jarofgreen.co.uk>
2014-03-04 18:57:08 +0000

    Rename

--- app/config.dist.php
+++ app/config.dist.php
@@ -6,9 +6,9 @@ define('DEBUG_MODE', true);
 
 define('DATABASE_TYPE', 'mysql');
 define('DATABASE_SERVER', 'localhost');
-define('DATABASE_USER', 'codepicturejudge');
+define('DATABASE_USER', 'youjudge');
 define('DATABASE_PASSWORD', 'password');
-define('DATABASE_NAME', 'codeapicturejudge');
+define('DATABASE_NAME', 'youjudge');
 
 
 
